30eba788562b041334a445e6965310542250cd32
iroja003 <rojas.ibar@gmail.com>
2023-06-26 04:49:06 -0400

    trece  commit

--- src/pages/Detalle.jsx
+++ src/pages/Detalle.jsx
@@ -1,5 +1,5 @@
 //
-import React, { useContext,useEffect  } from 'react'
+import React, { useContext,useEffect,  useState  } from 'react'
 import { useParams } from 'react-router-dom';
 //
 import { ProductContext } from '../context/ProductContext';
@@ -7,71 +7,44 @@ import { ProductContext } from '../context/ProductContext';
 import {   FaShoppingCart } from 'react-icons/fa';
 //
 const Detalle = () => {
-  
+  const {products, addToCart } = useContext(ProductContext);
+  const [prodDetail, setProdDetail] = useState({});
+  const [loading, setLoading] = useState(true);
   const {itemId} = useParams();
   console.log(itemId);
-  const {products, setProducts, addToCart } = useContext(ProductContext);
 
-  const getElemento = () => {
-      const selectProd = products.filter((p) => p.id === itemId).map((p) => p );
+  const getElemento =  () => {
+      setLoading(true) ;
+      const  selectProd =  products.find((p) => p.id === parseInt(itemId));
       console.log(selectProd);
-      setProducts(selectProd);
-     }
+      setProdDetail(selectProd);
+      setLoading(false);
+   };
 
    useEffect (() => {
     getElemento();
- },[itemId]);   
-    
-/*
-      <div className="container">
-        <div className="card mb-3 prod-det">
-            <div className="row g-0" >
-              <div className="col-md-4">
-                <img src=""
-                  className="img-fluid rounded-start" 
-                  alt=""
-                />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">titulo</h5>
-                  <p className="card-text">descripcion</p>
-                  <p className="card-text"><small className="text-muted">$ 75.000 </small></p>
-                  <button 
-                      className="btn btn-primary btn_card"
-                      type = "button"
-                      onClick={ handleClick()}
-                      >
-                        Add Carro
-                    </button>
-                </div>
-              </div>
-            </div>
-        </div>
-      </div> 
+   },[itemId]);   
 
+   if (loading) {
+      return(<h1>Loading ....</h1>)
+   }
 
-            <div className="container">
-          <h1> Producto : {itemId}</h1>
-      </div>
-*/
-
-  return (
+   return (
     <>
       <div className="container">
-        <div className="card mb-3 prod-det">
+        <div className="card mb-3 prod-det" key={prodDetail.id}>
             <div className="row g-0" >
               <div className="col-md-4">
-                <img src="/tennis1.jpg"
+                <img src={prodDetail.img}
                   className="img-fluid rounded-start" 
-                  alt=""
+                  alt={prodDetail.title}
                 />
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">{itemId}</h5>
-                  <p className="card-text">descripcion : {itemId}</p>
-                  <p className="card-text"><small className="text-muted">$ 75.000 : {itemId} </small></p>
+                  <h5 className="card-title">{prodDetail.title}</h5>
+                  <p className="card-text">{itemId}-{prodDetail.description}</p>
+                  <p className="card-text"><small className="text-muted">$ 75.000 : {prodDetail.price} </small></p>
                   <button 
                       className="btn btn-primary btn_card"
                       type = "button"
